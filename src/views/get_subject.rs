4617d8fe7830e327adb522045b2ec65b0e5b4837
Jesse Jones <jesse9jones@gmail.com>
2012-07-06 09:36:41 -0700

    got rid of subject_utils.rs

--- src/views/get_subject.rs
+++ src/views/get_subject.rs
@@ -1,9 +1,49 @@
 // Provides a view onto the back-end model used by gnos. This is not intended
 // to be something used very often: it's just a convenient mechanism by which
 // users can inspect the raw data used by the other views..
+import rrdf::object::*;
 import rrdf::object::object_methods;
 import rrdf::solution::solution_row_methods;
 import mustache::to_mustache;
+
+export get_subject;
+
+type str_map = std::map::hashmap<str, mustache::data>;
+
+fn append_uri(context: str_map, url: str, name: str)
+{
+	let mut urls = ~[];
+	
+	let map = std::map::str_hash();
+	map.insert("url", mustache::str(#fmt["/subject/%s", url]));
+	map.insert("text", mustache::str(name));
+	vec::push(urls, map);
+	
+	context.insert("urls", urls.to_mustache());
+}
+
+fn append_normal(context: str_map, value: str)
+{
+	context.insert("urls", false.to_mustache());
+	context.insert("normal", mustache::str(value));
+}
+
+fn object_to_context(object: object, context: str_map)
+{
+	// TODO: do we want to special case containers?
+	alt object
+	{
+		iri_value(_)			{append_uri(context, object.to_str(), object.to_str())}
+		blank_value(_)		{append_uri(context, object.to_str(), object.to_str())}
+		
+		// TODO: need to special case these and use some error css
+		unbound_value(_)	{append_normal(context, object.to_str())}
+		invalid_value(*)		{append_normal(context, object.to_str())}
+		error_value(_)		{append_normal(context, object.to_str())}
+		
+		_						{append_normal(context, object.to_str())}
+	}
+}
 
 fn get_subject(state_chan: comm::chan<msg>, request: server::request, response: server::response) -> server::response
 {
@@ -29,7 +69,7 @@ fn get_subject(state_chan: comm::chan<msg>, request: server::request, response: 
 		let map = std::map::str_hash();
 		map.insert("row-class", mustache::str(if index & 1u == 0u {"even"} else {"odd"}));
 		map.insert("predicate", mustache::str(predicate));
-		subject_utils::object_to_context(object, map);
+		object_to_context(object, map);
 		vec::push(predicates, mustache::map(map));
 	}
 	
