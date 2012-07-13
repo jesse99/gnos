2cd460470041d5162e5b397c7d6d98b96df81e08
Jesse Jones <jesse9jones@gmail.com>
2012-07-12 21:01:43 -0700

    Removed some obsolete commented out code

--- src/handlers/get_subjects.rs
+++ src/handlers/get_subjects.rs
@@ -6,30 +6,8 @@ import mustache::to_mustache;
 
 fn get_subjects(_state_chan: comm::chan<msg>, _request: server::request, response: server::response) -> server::response
 {
-//	let rows = get_state(state_chan, "
-//		PREFIX gnos: <http://www.gnos.org/2012/schema#>
-//		SELECT DISTINCT
-//			?name
-//		WHERE
-//		{
-//			?subject ?predicate ?object .
-//			BIND(rrdf:pname(?subject) AS ?name)
-//		} ORDER BY ?name");
-//
-//	let mut subjects = [];
-//	
-//	for vec::eachi(rows)
-//	|index, row|
-//	{
-//		let name = row.get("name").as_str();
-//		
-//		let map = std::map::str_hash();
-//		map.insert("row-class", mustache::str(if index & 1u == 0u {"even"} else {"odd"}));
-//		map.insert("url", mustache::str(#fmt["/subject/%s", name]));
-//		map.insert("text", mustache::str(name));
-//		vec::push(subjects, mustache::map(map));
-//	}
-	
-	//response.context.insert("subjects", mustache::vec(subjects));
+	// There isn't much for us to do here: all the heavy lifting is done on
+	// the client via javascript that uses server-sent events to dynamically
+	// update the view based on a SPARQL query.
 	{template: "subjects.html" with response}
 }
