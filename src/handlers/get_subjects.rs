ad4a76351a54d567ee8e78aea4cad32497aafc3d
Jesse Jones <jesse9jones@gmail.com>
2012-07-14 14:08:36 -0700

    Bit of cleanup

--- src/handlers/get_subjects.rs
+++ src/handlers/get_subjects.rs
@@ -1,10 +1,7 @@
 // Provides a view onto the back-end model used by gnos. This is not intended
 // to be something used very often: it's just a convenient mechanism by which
 // users can inspect the raw data used by the other views.
-import rrdf::*;
-import mustache::to_mustache;
-
-fn get_subjects(_state_chan: comm::chan<msg>, _request: server::request, response: server::response) -> server::response
+fn get_subjects(response: server::response) -> server::response
 {
 	// There isn't much for us to do here: all the heavy lifting is done on
 	// the client via javascript that uses server-sent events to dynamically
