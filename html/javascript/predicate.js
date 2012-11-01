bb2f0ee13c2df1e2bdbbb2536d74a78e84af3041
Jesse Jones <jesse9jones@gmail.com>
2012-11-01 07:48:49 -0700

    Finished predicate tokenizer

--- html/javascript/predicate.js
+++ html/javascript/predicate.js
@@ -12,14 +12,20 @@
 // len, and, etc		{type: 'keyword', value: 'and'}
 function parse_predicate(expr)
 {
+	// Not sure what's going on but \w and \. are not working, maybe
+	// because I'm using a Chrome beta.
 	var parts = [
-		' +',				// whitespace
-		'true',			// boolean
-		'false',			// boolean
+		' +',				// whitespace (not returned)
+		'true',			// Boolean
+		'false',			// Boolean
 		'[0-9]+',		// Number
 		"'[^'\n\r]*'",	// String
 		'"[^"\n\r]*"',	// String
-		'\\+|-|\\*|/|\\%|==|!=|<|<=|>=|>'	// operator
+		'is_empty|is_not_empty|len|not|to_num|to_lower|to_str|to_upper',		// unary operator (Object)
+		'\\+|-|\\*|/|\\%|==|!=|<=|>=|<|>|and|or|contains|ends_with|starts_with',	// binary operator (Object)
+		'if',																		// ternary operator (Object)
+		'concat|log',															// variadic operator (Object)
+		'[a-zA-Z][a-zA-Z_]*[.][a-zA-Z_][a-zA-Z]*'							// member (Object)
 	];
 	parts = parts.map(function (x) {return '(' + x + ')';});
 	var re = new RegExp(parts.join('|'), "gm");	// unfortunately there is no verbose option
@@ -40,7 +46,15 @@ function parse_predicate(expr)
 		else if (match[6])
 			result.push(match[6].slice(1, match[6].length - 1));
 		else if (match[7])
-			result.push({type: 'operator', value: match[7]});
+			result.push({type: 'unary', value: match[7]});
+		else if (match[8])
+			result.push({type: 'binary', value: match[8]});
+		else if (match[9])
+			result.push({type: 'ternary', value: match[9]});
+		else if (match[10])
+			result.push({type: 'variadic', value: match[10]});
+		else if (match[11])
+			result.push({type: 'member', target: match[11].split('.')[0], member: match[11].split('.')[1]});
 		else
 			throw SyntaxError("failed to parse '{0}'".format(expr));
 			
--- html/javascript/tests.js
+++ html/javascript/tests.js
@@ -2,6 +2,18 @@
 "use strict";
 
 // http://api.qunitjs.com/category/assert/
+function fails(expr)
+{
+	try
+	{
+		var result = parse_predicate(expr);
+		ok(false, "'{0}' parsed as {1:j}".format(expr, result));
+	}
+	catch (e)
+	{
+	}
+}
+
 test("escapeHtml", function()
 {
 	equal(escapeHtml("hello world"), "hello world");
@@ -15,19 +27,25 @@ test("parse_predicate", function()
 	deepEqual(parse_predicate('false   true'), [false, true]);
 	
 	deepEqual(parse_predicate('42'), [42]);
-
+	
 	deepEqual(parse_predicate('"foo"'), ['foo']);
 	deepEqual(parse_predicate("'foo bar'"), ['foo bar']);
 	
-	console.log('pp = {0:j}'.format(parse_predicate('"+ =="')));
-	deepEqual(parse_predicate('+ =='), [{type: 'operator', value: '+'}, {type: 'operator', value: '=='}]);
-
-	try
-	{
-		strictEqual(parse_predicate('false *** true'), null);
-		ok(false);
-	}
-	catch (e)
-	{
-	}
+	deepEqual(parse_predicate('len to_str'), [{type: 'unary', value: 'len'}, {type: 'unary', value: 'to_str'}]);
+	deepEqual(parse_predicate('+ =='), [{type: 'binary', value: '+'}, {type: 'binary', value: '=='}]);
+	deepEqual(parse_predicate("if"), [{type: 'ternary', value: 'if'}]);
+	deepEqual(parse_predicate("log concat"), [{type: 'variadic', value: 'log'}, {type: 'variadic', value: 'concat'}]);
+	
+	deepEqual(parse_predicate("foo.bar"), [{type: 'member', target: 'foo', member: 'bar'}]);
+	
+	deepEqual(parse_predicate("options.OSPF '10.1.0.1' selection.name == and"), [
+		{type: 'member', target: 'options', member: 'OSPF'},
+		'10.1.0.1',
+		{type: 'member', target: 'selection', member: 'name'},
+		{type: 'binary', value: '=='},
+		{type: 'binary', value: 'and'}
+	]);
+	
+	fails('foo');
+	fails('false *** true');
 });
