28ae1a3b4a67eaf71ba32eafda3f457ae44d686f
Jesse Jones <jesse9jones@gmail.com>
2012-11-02 17:52:15 -0700

    Moved some code into base_modeler

---
+++ scripts/base_modeler.py
@@ -0,0 +1,142 @@
+#!/usr/bin/python
+# This script uses snmp to periodically model a network, encodes it into a json 
+# dictionary, and ships the dictionary off to gnos using an http POST.  This
+# is designed to be a generic modeler suitable for pretty much any device 
+# running SNMP. Other modelers can be used to model more specialized
+# functionality (like OSPF and PIM).
+#
+# We use a Python script instead of simply doing this within gnos for a few
+# different reasons:
+# 1) There are already Python snmp wrapper libraries. (This was written when
+# the code still used pysnmp. Unfortunately pysnmp is not documented very well
+# once you start doing anything sophisticated. It also has a rather ridiculous API).
+# 2) Using a separate script will make it easier for gnos to manage multiple LANs.
+# 3) This separation simplifies development. In particular gnos can run on a 
+# developer machine and the script can run on an arbitrary machine connected
+# to an arbitrary LAN.
+# 4) This design makes it easy for users to write custom modelers using ssh
+# or whatever.
+import json, socket
+
+logger = None
+
+def set_logger(x):
+	global logger
+	logger = x
+
+def add_label(data, target, label, key, level = 0, style = ''):
+	if label:
+		sort_key = '%s-%s' % (level, key)
+		if style:
+			data['labels'].append({'target-id': target, 'label': label, 'level': level, 'sort-key': sort_key, 'style': style})
+		else:
+			data['labels'].append({'target-id': target, 'label': label, 'level': level, 'sort-key': sort_key})
+		
+def add_gauge(data, target, label, value, level, style, sort_key):
+	data['gauges'].append({'entity-id': target, 'label': label, 'value': value, 'level': level, 'style': style, 'sort-key': sort_key})
+
+def add_details(data, target, label, details, opened, sort_key, key):
+	data['details'].append({'entity-id': target, 'label': label, 'details': json.dumps(details), 'open': opened, 'sort-key': sort_key, 'id': key})
+
+def add_relation(data, left, right, style = '', left_label = None, middle_label = None, right_label = None):
+	relation = {'left-entity-id': left, 'right-entity-id': right, 'style': style}
+	if left_label:
+		relation['left-label'] = left_label
+	if middle_label:
+		relation['middle-label'] = middle_label
+	if right_label:
+		relation['right-label'] = right_label
+	data['relations'].append(relation)
+
+def open_alert(data, target, key, mesg, resolution, kind):
+	data['alerts'].append({'entity-id': target, 'key': key, 'mesg': mesg, 'resolution': resolution, 'kind': kind})
+
+def close_alert(data, target, key):
+	data['alerts'].append({'entity-id': target, 'key': key})
+
+def secs_to_str(secs):
+	if secs >= 365.25*86400:
+		return '%.2f years' % (secs/(365.25*86400))		# http://en.wikipedia.org/wiki/Month#Month_lengths
+	elif secs >= 365.25*86400/12:
+		return '%.2f months' % (secs/(365.25*86400/12))
+	elif secs >= 86400:
+		return '%.1f days' % (secs/(86400))
+	elif secs >= 60*60:
+		return '%.1f hours' % (secs/(60*60))
+	elif secs >= 60:
+		return '%.0f minutes' % (secs/(60))
+	elif secs >= 1:
+		return '%.0f seconds' % secs
+	else:
+		return '%.3f msecs' % (1000*secs)
+			
+def get_subnet(s):
+	def count_leading_ones(mask):
+		count = 0
+		
+		bit = 1 << 31
+		while bit > 0:
+			if mask & bit == bit:
+				count += 1
+				bit >>= 1
+			else:
+				break
+		
+		return count
+	
+	def count_trailing_zeros(mask):
+		count = 0
+		
+		bit = 1
+		while bit < (1 << 32):
+			if mask & bit == 0:
+				count += 1
+				bit <<= 1
+			else:
+				break
+		
+		return count;
+	
+	if s:
+		parts = s.split('.')
+		bytes = map(lambda p: int(p), parts)
+		mask = reduce(lambda sum, current: 256*sum + current, bytes, 0)
+		leading = count_leading_ones(mask)
+		trailing = count_trailing_zeros(mask)
+		if leading + trailing == 32:
+			return leading
+		else:
+			return s		# unusual netmask where 0s and 1s are mixed.
+	else:
+		'?'
+
+def send_update(config, connection, data):
+	logger.debug("sending update")
+	logger.debug("%s" % json.dumps(data, sort_keys = True, indent = 4))
+	if connection:
+		try:
+			body = json.dumps(data)
+			headers = {"Content-type": "application/json", "Accept": "text/html"}
+			
+			connection.request("PUT", config['path'], body, headers)
+			response = connection.getresponse()
+			response.read()			# we don't use this but we must call it (or, on the second call, we'll get ResponseNotReady errors)
+			if not str(response.status).startswith('2'):
+				logger.error("Error PUTing: %s %s" % (response.status, response.reason))
+				raise Exception("PUT failed")
+		except Exception as e:
+			address = "%s:%s" % (config['server'], config['port'])
+			logger.error("Error PUTing to %s:%s: %s" % (address, config['path'], e), exc_info = type(e) != socket.error)
+			raise Exception("PUT failed")
+
+def send_entities(config, connection):
+	entities = []
+	for (name, device) in config["devices"].items():
+		if device['type'] == "router":
+			style = "font-size:larger font-weight:bolder"
+		else:
+			style = ""
+		entity = {"id": device['ip'], "label": name, "style": style}
+		logger.debug("entity: %s" % entity)
+		entities.append(entity)
+	send_update(config, connection, {"modeler": "config", "entities": entities})
--- scripts/snmp-modeler.py
+++ scripts/snmp-modeler.py
@@ -16,15 +16,17 @@
 # to an arbitrary LAN.
 # 4) This design makes it easy for users to write custom modelers using ssh
 # or whatever.
-import cgi, json, itertools, httplib, logging, logging.handlers, re, socket, subprocess, sys, threading, time
+import cgi, json, itertools, httplib, logging, logging.handlers, re, subprocess, sys, threading, time
+from base_modeler import *
 
 try:
 	import argparse
 except:
 	sys.stderr.write("This script requires Python 2.7 or later\n")
 	sys.exit(2)
-
+	
 logger = logging.getLogger('snmp-modeler')
+set_logger(logger)
 connection = None
 
 # http://tools.cisco.com/Support/SNMP/do/BrowseOID.do?objectInput=sysDescr&translate=Translate&submitValue=SUBMIT&submitClicked=true
@@ -329,36 +331,6 @@ def process_device(admin_ip, data, contents, context):
 		if level:
 			add_gauge(data, target, 'processor load', value, level, style, sort_key = 'y')
 
-def add_label(data, target, label, key, level = 0, style = ''):
-	if label:
-		sort_key = '%s-%s' % (level, key)
-		if style:
-			data['labels'].append({'target-id': target, 'label': label, 'level': level, 'sort-key': sort_key, 'style': style})
-		else:
-			data['labels'].append({'target-id': target, 'label': label, 'level': level, 'sort-key': sort_key})
-		
-def add_gauge(data, target, label, value, level, style, sort_key):
-	data['gauges'].append({'entity-id': target, 'label': label, 'value': value, 'level': level, 'style': style, 'sort-key': sort_key})
-
-def add_details(data, target, label, details, opened, sort_key, key):
-	data['details'].append({'entity-id': target, 'label': label, 'details': json.dumps(details), 'open': opened, 'sort-key': sort_key, 'id': key})
-
-def add_relation(data, left, right, style = '', left_label = None, middle_label = None, right_label = None):
-	relation = {'left-entity-id': left, 'right-entity-id': right, 'style': style}
-	if left_label:
-		relation['left-label'] = left_label
-	if middle_label:
-		relation['middle-label'] = middle_label
-	if right_label:
-		relation['right-label'] = right_label
-	data['relations'].append(relation)
-
-def open_alert(data, target, key, mesg, resolution, kind):
-	data['alerts'].append({'entity-id': target, 'key': key, 'mesg': mesg, 'resolution': resolution, 'kind': kind})
-
-def close_alert(data, target, key):
-	data['alerts'].append({'entity-id': target, 'key': key})
-
 # In general lines look like:
 #    IP-MIB::icmpOutErrors.0 0
 #    TCP-MIB::tcpConnLocalAddress[127.0.0.1][2601][0.0.0.0][0] 127.0.0.1
@@ -399,22 +371,6 @@ def dump_snmp(ip, name, contents):
 	logger.debug('%s %s:' % (ip, name))
 	logger.debug('%s' % (contents))
 		
-def secs_to_str(secs):
-	if secs >= 365.25*86400:
-		return '%.2f years' % (secs/(365.25*86400))		# http://en.wikipedia.org/wiki/Month#Month_lengths
-	elif secs >= 365.25*86400/12:
-		return '%.2f months' % (secs/(365.25*86400/12))
-	elif secs >= 86400:
-		return '%.1f days' % (secs/(86400))
-	elif secs >= 60*60:
-		return '%.1f hours' % (secs/(60*60))
-	elif secs >= 60:
-		return '%.0f minutes' % (secs/(60))
-	elif secs >= 1:
-		return '%.0f seconds' % secs
-	else:
-		return '%.3f msecs' % (1000*secs)
-		
 # MAC addresses are normally lower case which is kind of ugly and
 # also are not always two digits which is also ugly, but even more
 # important causes problems when we try to match up information
@@ -433,77 +389,6 @@ def add_units(value, unit):
 	if value or type(value) == float:
 		value = '%s %s' % (value, unit)
 	return value
-			
-def count_leading_ones(mask):
-	count = 0
-	
-	bit = 1 << 31
-	while bit > 0:
-		if mask & bit == bit:
-			count += 1
-			bit >>= 1
-		else:
-			break
-	
-	return count
-
-def count_trailing_zeros(mask):
-	count = 0
-	
-	bit = 1
-	while bit < (1 << 32):
-		if mask & bit == 0:
-			count += 1
-			bit <<= 1
-		else:
-			break
-	
-	return count;
-
-def get_subnet(s):
-	if s:
-		parts = s.split('.')
-		bytes = map(lambda p: int(p), parts)
-		mask = reduce(lambda sum, current: 256*sum + current, bytes, 0)
-		leading = count_leading_ones(mask)
-		trailing = count_trailing_zeros(mask)
-		if leading + trailing == 32:
-			return leading
-		else:
-			return s		# unusual netmask where 0s and 1s are mixed.
-	else:
-		'?'
-
-def send_update(config, data):
-	logger.debug("sending update")
-	logger.debug("%s" % json.dumps(data, sort_keys = True, indent = 4))
-	if connection:
-		try:
-			body = json.dumps(data)
-			headers = {"Content-type": "application/json", "Accept": "text/html"}
-			
-			connection.request("PUT", config['path'], body, headers)
-			response = connection.getresponse()
-			response.read()			# we don't use this but we must call it (or, on the second call, we'll get ResponseNotReady errors)
-			if not str(response.status).startswith('2'):
-				logger.error("Error PUTing: %s %s" % (response.status, response.reason))
-				raise Exception("PUT failed")
-		except Exception as e:
-			address = "%s:%s" % (config['server'], config['port'])
-			logger.error("Error PUTing to %s:%s: %s" % (address, config['path'], e), exc_info = type(e) != socket.error)
-			raise Exception("PUT failed")
-
-def send_entities(config):
-	entities = []
-	for (name, device) in config["devices"].items():
-		if device['type'] == "router":
-			style = "font-size:larger font-weight:bolder"
-		else:
-			style = ""
-		entity = {"id": device['ip'], "label": name, "style": style}
-		logger.debug("entity: %s" % entity)
-		entities.append(entity)
-	send_update(config, {"modeler": "config", "entities": entities})
 
 def run_process(command):
 	process = subprocess.Popen(command, bufsize = 8*1024, shell = True, stdout = subprocess.PIPE, stderr = subprocess.PIPE)
@@ -577,7 +462,7 @@ class Poll(object):
 				self.__add_bandwidth_details(data, 'in')
 			self.__add_system_info(data);
 			self.__add_interface_uptime_alert(data)
-			send_update(self.__config, data)
+			send_update(self.__config, connection, data)
 			
 			elapsed = time.time() - self.__current_time
 			self.__last_time = self.__current_time
@@ -808,7 +693,7 @@ if args.put:
 
 try:
 	# Send entity information to the server.
-	send_entities(config)
+	send_entities(config, connection)
 	
 	# Start polling each device.
 	poller = Poll(args, config)
