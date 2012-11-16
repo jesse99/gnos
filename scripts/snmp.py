cf606e3386ac69283da858b9d0e9f7921776d347
Jesse Jones <jesse9jones@gmail.com>
2012-11-15 20:31:52 -0800

    Added support for ciscoEnvMonFanStatusTable

--- scripts/snmp.py
+++ scripts/snmp.py
@@ -353,7 +353,7 @@ def process_temp(data, contents, query):
 	names = get_values(contents, "ciscoEnvMonTemperatureStatusDescr")
 	status = get_values(contents, "ciscoEnvMonTemperatureStatusValue")
 	threshold = get_values(contents, "ciscoEnvMonTemperatureThreshold")
-	states = get_values(contents, "ciscoEnvMonTemperatureState")		# TODO: probably should add an alert if ciscoEnvMonTemperatureLastShutdown is non-zero
+	states = get_values(contents, "ciscoEnvMonTemperatureState")
 	for (key, name) in names.items():
 		current = int(status.get(key, 0))*9/5 + 32
 		maximum = int(threshold.get(key, 0))*9/5 + 32
@@ -362,17 +362,6 @@ def process_temp(data, contents, query):
 			state = states.get(key, '')
 			query.device.system_info += '* %s is %s F (%s). Shutdown will happen at %s F.\n' % (name, current, state, maximum)
 			
-			# add a gauge if the temperature is too high
-			level = None
-			if state == 'critical' or state == 'shutdown':
-				level = 1
-				style = 'gauge-bar-color:salmon'
-			elif state == 'warning':
-				level = 2
-				style = 'gauge-bar-color:darkorange'
-			if level:
-				add_gauge(data, target, name, float(current)/maximum, level, style, sort_key = 'zz')
-				
 			# add an alert if the temperature is too high
 			key = '%s-temp' % name
 			if state == 'critical' or state == 'shutdown':
@@ -380,6 +369,25 @@ def process_temp(data, contents, query):
 				open_alert(data, target, key, mesg = mesg, resolution = '', kind = 'error')
 			elif state == 'warning':
 				mesg = '%s temperature is %s F, shutdown will happen at %s F.' % (name, current, maximum)
+				open_alert(data, target, key, mesg = mesg, resolution = '', kind = 'warning')
+			else:
+				close_alert(data, target, key)
+
+# CISCO-ENVMON-MIB::ciscoEnvMonFanStatusDescr[1] Fan 1		one for each fan
+# CISCO-ENVMON-MIB::ciscoEnvMonFanState[1] normal			or warning, critical, shutdown, notPresent, notFunctioning
+def process_fan(data, contents, query):
+	target = 'entities:%s' % query.device.admin_ip
+	names = get_values(contents, "ciscoEnvMonFanStatusDescr")
+	states = get_values(contents, "ciscoEnvMonFanState")
+	for (key, name) in names.items():
+		state = states.get(key, '')
+		if state:
+			key = '%s-fan' % name
+			if state == 'critical' or state == 'shutdown':
+				mesg = '%s state is %s.' % (name, state)
+				open_alert(data, target, key, mesg = mesg, resolution = '', kind = 'error')
+			elif state == 'warning':
+				mesg = '%s state is %s.' % (name, state)
 				open_alert(data, target, key, mesg = mesg, resolution = '', kind = 'warning')
 			else:
 				close_alert(data, target, key)
@@ -532,6 +540,7 @@ class QueryDevice(object):
 			if mib == 'cisco-router':
 				add_if_missing(self.__mibs, 'ciscoFlashPartitions')
 				add_if_missing(self.__mibs, 'ciscoEnvMonTemperatureStatusTable')
+				add_if_missing(self.__mibs, 'ciscoEnvMonFanStatusTable')
 			elif mib == 'linux-router' or  mib == 'linux-host':
 				add_if_missing(self.__mibs, 'hrStorage')
 				add_if_missing(self.__mibs, 'hrDevice')
@@ -548,6 +557,7 @@ class QueryDevice(object):
 			'hrDevice': process_device,
 			'ciscoFlashPartitions': process_flash,
 			'ciscoEnvMonTemperatureStatusTable': process_temp,
+			'ciscoEnvMonFanStatusTable': process_fan,
 		}
 		self.device = device
 	
